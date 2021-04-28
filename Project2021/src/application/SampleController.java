4aa8ae6dca37a413b2e74f5c470b5c2613bcfeab
cy.andersen <cy.andersen@192.168.0.168>
2021-04-28 11:39:49 -0400

    28/04/2021

--- Project2021/src/application/SampleController.java
+++ Project2021/src/application/SampleController.java
@@ -1,5 +1,61 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class SampleController {
-	
+
+    @FXML
+    private TableColumn<?, ?> colservice;
+
+    @FXML
+    private Button buttonmod;
+
+    @FXML
+    private ComboBox<?> combostatus;
+
+    @FXML
+    private Button buttoneff;
+
+    @FXML
+    private Button buttonajo;
+
+    @FXML
+    private TableColumn<?, ?> colsexe;
+
+    @FXML
+    private ComboBox<?> comboresult;
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private Button buttonrec;
+
+    @FXML
+    private TextField txtnum;
+
+    @FXML
+    private TableColumn<?, ?> colresut;
+
+    @FXML
+    private TableColumn<?, ?> colnum;
+
+    @FXML
+    private TableColumn<?, ?> colstatus;
+
+    @FXML
+    private ComboBox<?> comboservice;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private TextField txtsexe;
+
 }
